72f1ab10671f64d90c6a29d96d6b7a5a76ac471b
Mykhailo <savkamykhaylo1@gmail.com>
2026-07-09 21:20:26 +0300

    Fixed error

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ class Person:
 
 def create_person_list(people: list) -> list:
 
+    person_list = [Person(person["name"], person["age"]) for person in people]
+
     for person in people:
         current = Person.people[person["name"]]
 
@@ -19,4 +21,4 @@ def create_person_list(people: list) -> list:
         if person.get("husband") is not None:
             current.husband = Person.people[person["husband"]]
 
-    return [Person(person["name"], person["age"]) for person in people]
+    return person_list
