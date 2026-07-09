0eaf46b90d202041728c37048631c9b688afb223
Mykhailo <savkamykhaylo1@gmail.com>
2026-07-09 21:13:47 +0300

    Fixed PR comments

--- app/main.py
+++ app/main.py
@@ -9,18 +9,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    result = []
-
-    for person in people:
-        result.append(Person(person["name"], person["age"]))
 
     for person in people:
         current = Person.people[person["name"]]
 
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife") is not None:
             current.wife = Person.people[person["wife"]]
 
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband") is not None:
             current.husband = Person.people[person["husband"]]
 
-    return result
+    return [Person(person["name"], person["age"]) for person in people]
